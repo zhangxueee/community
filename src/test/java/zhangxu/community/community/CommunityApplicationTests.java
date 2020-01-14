75b3674f2128dfd051c0bf3958beacb69a88a69a
zhangxu <zhangxu@himoca.com>
2020-01-14 20:54:43 +0800

    init repo

---
+++ src/test/java/zhangxu/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package zhangxu.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
